3efd8a3180e58dbb2066971ec269d85452073646
0BAB1 <hb.hugobabin@gmail.com>
2021-06-29 09:44:12 +0200

    now a basic mechanic helper

--- MenuWindow.xaml.cs
+++ MenuWindow.xaml.cs
@@ -21,19 +21,5 @@ namespace ProjetUI
         {
             InitializeComponent();
         }
-
-        private void buttonCalcul_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                int valueOne = int.Parse(numberOne.Text);
-                int valueTwo = int.Parse(numberTwo.Text);
-                result.Text = "bg";
-            }
-            catch(Exception ex)
-            {
-                result.Text = "mauvaise input";
-            }
-        }
     }
 }
